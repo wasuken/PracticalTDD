c1d5ec2d7fe0e1239de312b75350301110d419a5
wasuken <wevorence@gmail.com>
2017-11-15 22:08:23 +0900

    P99,13章まで終了
    続きは明日...

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -7,7 +7,10 @@ class Money implements Expression{
 		return new Money(amount * multiplier,currency);
 	}
 	Expression plus(Money addend) {
-		return new Money(amount + addend.amount,currency);
+		return new Sum(this,addend);
+	}
+	public	Money reduce(String to) {
+		return this;
 	}
 	Money(int amount,String currency){
 		this.amount = amount;
--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -31,4 +31,18 @@ public class MoneyTest {
 		Money reduced = bank.reduce(sum,"USD");
 		assertEquals(Money.dollar(10), reduced);
 	}
+	@Test
+	public void testPlusReturnsSum() {
+		Money five = Money.dollar(5);
+		Expression result = five.plus(five);
+		Sum sum = (Sum)result;
+		assertEquals(five, sum.augend);
+		assertEquals(five, sum.addend);
+	}
+	@Test
+	public void testReduceSum() {
+		Bank bank = new Bank();
+		Money result = bank.reduce(Money.dollar(1), "USD");
+		assertEquals(Money.dollar(1), result);
+	}
 }
