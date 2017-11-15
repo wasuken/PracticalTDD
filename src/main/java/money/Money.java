063e0685836281ef88c0a7f898416046bda4c3c6
wasuken <wevorence@gmail.com>
2017-11-15 21:45:23 +0900

    ８６ページまで進めた。
    いちお緑キープ。当たり前なんだけどね。

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -1,9 +1,14 @@
 package money;
 
-public abstract class Money {
+class Money implements Expression{
 	protected int amount;
 	protected String currency;
-	abstract Money times(int multiplier);
+	Money times(int multiplier) {
+		return new Money(amount * multiplier,currency);
+	}
+	Expression plus(Money addend) {
+		return new Money(amount + addend.amount,currency);
+	}
 	Money(int amount,String currency){
 		this.amount = amount;
 		this.currency = currency;
@@ -13,12 +18,15 @@ public abstract class Money {
 	}
 	public boolean equals(Object object){
 		Money money = (Money)object;
-		return money.amount == this.amount && getClass().equals(money.getClass());
+		return money.amount == this.amount && currency().equals(money.currency());
 	}
 	static Money dollar(int amount){
-		return new Dollar(amount,"USD");
+		return new Money(amount,"USD");
 	}
 	static Money franc(int amount){
-		return new Franc(amount,"CHF");
+		return new Money(amount,"CHF");
+	}
+	public String toString() {
+		return amount + " " + currency;
 	}
 }
--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -13,15 +13,7 @@ public class MoneyTest {
 		assertEquals(Money.dollar(15), five.times(3));
 	}
 	@Test
-	public void testFrancMultiplication(){
-		Money five = Money.franc(5);
-		assertEquals(Money.franc(10), five.times(2));
-		assertEquals(Money.franc(15), five.times(3));
-	}
-	@Test
 	public void testEquality(){
-		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
-		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 		assertTrue(Money.franc(5).equals(Money.franc(5)));
 		assertFalse(Money.franc(5).equals(Money.franc(6)));
 		assertFalse(Money.franc(5).equals(Money.dollar(5)));
@@ -30,5 +22,13 @@ public class MoneyTest {
 	public void testCurrency(){
 		assertEquals("USD", Money.dollar(1).currency());
 		assertEquals("CHF", Money.franc(1).currency());
+	}
+	@Test
+	public void testSimpleAddition() {
+		Money five = Money.dollar(5);
+		Expression sum = five.plus(five);
+		Bank bank = new Bank();
+		Money reduced = bank.reduce(sum,"USD");
+		assertEquals(Money.dollar(10), reduced);
 	}
 }
