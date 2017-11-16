ddc91f0cf950dbf71f2f4a7793759e0e0eae71d7
wasuken <wevorence@gmail.com>
2017-11-16 20:58:20 +0900

    P111まで終わり。
    hashCodeがhashcodeになってたの気が付かなくて３０分溶けたの草。

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -3,18 +3,19 @@ package money;
 class Money implements Expression{
 	protected int amount;
 	protected String currency;
+	Money(int amount,String currency){
+		this.amount = amount;
+		this.currency = currency;
+	}
 	Money times(int multiplier) {
 		return new Money(amount * multiplier,currency);
 	}
 	Expression plus(Money addend) {
 		return new Sum(this,addend);
 	}
-	public	Money reduce(String to) {
-		return this;
-	}
-	Money(int amount,String currency){
-		this.amount = amount;
-		this.currency = currency;
+	public	Money reduce(Bank bank,String to) {
+		int rate = bank.rate(currency,to);
+		return new Money(amount / rate, to);
 	}
 	String currency(){
 		return currency;
--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -41,8 +41,26 @@ public class MoneyTest {
 	}
 	@Test
 	public void testReduceSum() {
+		Expression sum = new Sum(Money.dollar(3),Money.dollar(4));
+		Bank bank = new Bank();
+		Money result = bank.reduce(sum, "USD");
+		assertEquals(Money.dollar(7), result);
+	}
+	@Test
+	public void testReduceMoney() {
 		Bank bank = new Bank();
 		Money result = bank.reduce(Money.dollar(1), "USD");
 		assertEquals(Money.dollar(1), result);
+	}
+	@Test
+	public void testReduceMoneyDifferentCurrency() {
+		Bank bank = new Bank();
+		bank.addRate("CHF","USD",2);
+		Money result = bank.reduce(Money.franc(2), "USD");
+		assertEquals(Money.dollar(1), result);
+	}
+	@Test
+	public void testIdentityRate() {
+		assertEquals(1,new Bank().rate("USD", "USD"));
 	}
 }
